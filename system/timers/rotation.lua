3ca3083cedaf8412c28d298ccc1fe14811d5e784
rootlsuer <rootluser@gmail.com>
2013-11-13 05:58:00 -1000

    reset sticky targeting cvar back to user setting after cycle

--- system/timers/rotation.lua
+++ system/timers/rotation.lua
@@ -11,7 +11,7 @@ ProbablyEngine.cycle = function(skip_verify)
     and ProbablyEngine.module.player.specId
 
   if cycle or skip_verify then
-
+    local stickyValues = GetCVar("deselectOnClick")
     local spell, target = ''
 
     --[[ Disabled for now...
@@ -68,8 +68,8 @@ ProbablyEngine.cycle = function(skip_verify)
       end
 
     end
+    SetCVar("deselectOnClick", stickyValue)
   end
-
 end
 
 ProbablyEngine.timer.register("rotation", function()
@@ -85,6 +85,7 @@ ProbablyEngine.timer.register("oocrotation", function()
     and ProbablyEngine.rotation.activeOOCRotation ~= false
 
   if cycle then
+    local stickyValues = GetCVar("deselectOnClick")
     local spell, target = ''
     spell, target = ProbablyEngine.parser.table(ProbablyEngine.rotation.activeOOCRotation, 'player')
     if target == nil then target = 'player' end
@@ -123,5 +124,6 @@ ProbablyEngine.timer.register("oocrotation", function()
         end
       end
     end
+    SetCVar("deselectOnClick", stickyValue)
   end
 end, 500)
