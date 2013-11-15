3b2f266705d06e14cb422a69b68b27eb38e111b6
Ben Phelps <bphelpsen@gmail.com>
2013-11-15 16:13:45 -0600

    fix cast_channel listener
    how has this gone so long unnoticed

--- system/listeners/unit_spellcast_channel_start.lua
+++ system/listeners/unit_spellcast_channel_start.lua
@@ -1,9 +1,10 @@
 -- ProbablyEngine Rotations - https://probablyengine.com/
 -- Released under modified BSD, see attached LICENSE.
 
-ProbablyEngine.listener.register("UNIT_SPELLCAST_SUCCEEDED", function(...)
+ProbablyEngine.listener.register("UNIT_SPELLCAST_CHANNEL_START", function(...)
   local unitID = ...
   if unitID == "player" then
-    ProbablyEngine.module.player.casting = false
+    ProbablyEngine.module.player.casting = true
+    ProbablyEngine.parser.lastCast = UnitCastingInfo("player")
   end
 end)
