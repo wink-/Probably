982de18b183d11a496f628e8901acb22634c96a8
Ben Phelps <bphelpsen@gmail.com>
2013-11-16 16:16:49 -0600

    fixes last modifier for melee folks

---
+++ system/listeners/unit_spellcast_sent.lua
@@ -0,0 +1,12 @@
+-- ProbablyEngine Rotations - https://probablyengine.com/
+-- Released under modified BSD, see attached LICENSE.
+
+ProbablyEngine.listener.register("UNIT_SPELLCAST_SENT", function(...)
+  local unitID, spell = ...
+  if unitID == "player" then
+    if ProbablyEngine.module.queue.queue == spell then
+      ProbablyEngine.module.queue.queue = nil
+    end
+    ProbablyEngine.parser.lastCast = spell
+  end
+end)
