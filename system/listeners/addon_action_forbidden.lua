f4f37fecf1416ea5cfc5884556d6395bbadf9d9b
Ben Phelps <bphelpsen@gmail.com>
2013-11-23 21:47:38 -0600

    use addon static for the name

--- system/listeners/addon_action_forbidden.lua
+++ system/listeners/addon_action_forbidden.lua
@@ -4,7 +4,7 @@
 ProbablyEngine.listener.register("ADDON_ACTION_FORBIDDEN", function(...)
   -- We can attempt to hide these without totally raping the UI
   local addon, event = ...
-  if addon == "Probably" then
+  if addon == ProbablyEngine.addonName then
     StaticPopup1:Hide()
     ProbablyEngine.full = false
     ProbablyEngine.debug.print("Event Forbidden: " .. event, 'action_block')
